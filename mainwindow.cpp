4f916f31fcbb02bd7e9190d993469cbf321e38b7
OIegator <64355448+OIegator@users.noreply.github.com>
2020-07-15 18:06:46 +0300

    Add status bar messages
    - Add messages for ad_pressed;
    - Add slots for button signals;
    - Try to delete row by selection;

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,6 +23,8 @@ MainWindow::MainWindow(QWidget *parent)
     myTableModel *myModel = new myTableModel();
     myModel->populateData(films);
     connect(ui->pushButton,SIGNAL(released()),this,SLOT(ab_pressed()));
+    connect(ui->pushButton_2,SIGNAL(released()),this,SLOT(del_pressed()));
+    connect(ui->pushButton_3,SIGNAL(released()),this,SLOT(fb_pressed()));
     ui->tableView->setModel(myModel);
 
 }
@@ -32,17 +34,33 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::ab_pressed(){
-    Film f(ui->lineEdit_2->text(),ui->lineEdit_3->text(),ui->lineEdit->text());
+void MainWindow::ab_pressed() {
+    if(ui->lineEdit->text() == "" || ui->lineEdit_2->text() == "" || ui->lineEdit_3->text() == "") {
+        ui->statusbar->showMessage("Ошибка. Все поля должны быть заполнены.");
+    } else { Film f(ui->lineEdit_2->text(),ui->lineEdit_3->text(),ui->lineEdit->text());
     films.append(f);
     myTableModel *myModel = new myTableModel();
     myModel->populateData(films);
     ui->tableView->setModel(myModel);
+    ui->lineEdit->clear();
+    ui->lineEdit_2->clear();
+    ui->lineEdit_3->clear();
+    ui->statusbar->showMessage("Запись добавлена.");
+    }
 }
-void MainWindow::del_pressed(){
-    ui->pushButton->setText("Нажата");
+void MainWindow::del_pressed() {
+    QItemSelectionModel *selectModel = ui->tableView->selectionModel();
+    QModelIndexList indexes = selectModel->selectedRows();
+    if (indexes.isEmpty()) {
+        ui->statusbar->showMessage("Выберите строки для их удаления!");
+        return;
+    }
+    foreach (const QModelIndex &index, indexes) {
+        ui->tableView->hideRow(index.row());
+        ui->statusbar->showMessage("Запись удалена.");
+    }
 }
 void MainWindow::fb_pressed(){
-    ui->pushButton->setText("Нажата");
+    ui->statusbar->showMessage("Нажата");
 }
 
